66340dce0c383435f14bea9a79e3008a1cda7c3d
Luiz Damas <luizdamasamorim@gmail.com>
2024-10-22 17:27:31 -0300

    Criando alguns arquivos com a linguagem C

---
+++ src/boasvindas.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    //  adicionar um pequeno cabeçario
+
+    printf("----------Programa mensagem----------");
+    printf ("\nSeja Bem Vindo ao nosso programa\n");
+    printf("---------------fim-------------------\n");
+
+    return 0;
+}
